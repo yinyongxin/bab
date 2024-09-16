3395c580a5838dc11e0b3174cb549a3827c240b5
银永鑫 <1101278643@qq.com>
2024-09-16 19:30:48 +0800

    feat: 目录结构

--- bab-server/src/main.ts
+++ bab-server/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules.ts/app/app.module';
 import { initSwagger } from './plugins/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  initSwagger(app)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  initSwagger(app);
   await app.listen(3000);
 }
 
--- bab-server/src/plugins/swagger/index.ts
+++ bab-server/src/plugins/swagger/index.ts
@@ -1,30 +1,28 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import {
   DocumentBuilder,
   SwaggerCustomOptions,
   SwaggerModule,
 } from '@nestjs/swagger';
+export const initSwagger = (app: NestExpressApplication) => {
+  const config = new DocumentBuilder();
+  config.setTitle('BAB服务器接口');
+  config.setDescription('BAB服务器接口');
+  config.setVersion('1.0.0');
 
-export const initSwagger = (app) => {
-  const config = new DocumentBuilder()
-    .setTitle('Sport')
-    .setDescription('The Sport API description')
-    .setVersion('1.0')
-    // 设置接口请求验证token(要启用基本身份验证还需再每个Controller添加装饰器ApiBearerAuth)
-    .addBearerAuth(
-      {
-        type: 'apiKey',
-        name: 'Authorization',
-        in: 'header',
-      },
-      'Authorization',
-    )
-    .setExternalDoc('Download JSON Specifications', 'http://localhost:3000/api-json')
-    .build()
-
-  const document = SwaggerModule.createDocument(
-    app,
-    config
+  // 设置接口请求验证token(要启用基本身份验证还需再每个Controller添加装饰器ApiBearerAuth)
+  config.addBearerAuth(
+    {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
+    },
+    'Authorization',
   );
+
+  config.setExternalDoc('下载JSON文件请点击', 'http://localhost:3000/api-json');
+
+  const document = SwaggerModule.createDocument(app, config.build());
 
   // Swaagger自定义设置
   const customOptions: SwaggerCustomOptions = {
@@ -33,7 +31,7 @@ export const initSwagger = (app) => {
       persistAuthorization: true,
     },
     // 自定义PageTitle
-    customSiteTitle: 'My API Docs',
+    customSiteTitle: 'BAB接口',
   };
 
   SwaggerModule.setup('/api', app, document, customOptions);
