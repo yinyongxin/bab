aebdf7dc85d57a9cabdc6e0ea133f2c979813002
银永鑫 <1101278643@qq.com>
2024-09-19 21:59:37 +0800

    feat: 热更新

--- bab-server/src/main.ts
+++ bab-server/src/main.ts
@@ -3,18 +3,24 @@ import { EnterModule } from './modules';
 import { initSwagger } from './plugins/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import middlewares from './middlewares';
-
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(EnterModule, {
     // 开启跨域
     cors: true,
   });
+  
   initSwagger(app);
   middlewares.forEach((middleware) => {
     app.use(middleware);
   });
 
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 bootstrap();
