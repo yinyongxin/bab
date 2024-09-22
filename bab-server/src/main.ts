64cb4e077f6caca7c85d9a6eb9c69cf7c3461842
银永鑫 <1101278643@qq.com>
2024-09-22 23:26:07 +0800

    feat: 无用代码

--- bab-server/src/main.ts
+++ bab-server/src/main.ts
@@ -19,9 +19,7 @@ async function bootstrap() {
   await app.listen(3000);
 
   if (module.hot) {
-    module.hot.accept(() => {
-      console.log('111', 111);
-    });
+    module.hot.accept();
     module.hot.dispose(() => app.close());
   }
 }
