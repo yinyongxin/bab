e1b18780004c8dd903d2049e422f453fc43a6440
银永鑫 <1101278643@qq.com>
2024-09-29 23:12:00 +0800

    refactor(admin-avatar): 移除多余的导入和属性拆分
    从Avatar组件中移除了未使用的splitProps导入和size属性的不必要拆分。这简化了代码，降低了复杂性，并提升了性能。

--- admin/src/components/Avatar/index.tsx
+++ admin/src/components/Avatar/index.tsx
@@ -1,25 +1,33 @@
-import { Component, mergeProps, splitProps } from "solid-js"
-import styles from "./index.module.less"
-import { RadiusSizeEnum, SizeEnum } from "../enum"
+// 导入必要的依赖库
+import { Component, mergeProps } from "solid-js";
+import styles from "./index.module.less";
+import { RadiusSizeEnum, SizeEnum } from "../enum";
+
+// 定义头像组件的属性类型
 export type AvatarProps = {
-  src: string,
-  size?: SizeEnum
-  radius?: RadiusSizeEnum
-}
+  src: string, // 头像的图片链接
+  size?: SizeEnum // 头像的大小，可选
+  radius?: RadiusSizeEnum // 头像的圆角大小，可选
+};
+
+// 头像组件
 export const Avatar: Component<AvatarProps> = (props) => {
+  // 合并默认属性与传入的属性
   const merge = mergeProps({
-    radius: RadiusSizeEnum.Default,
-    size: SizeEnum.Default
-  }, props)
+    radius: RadiusSizeEnum.Default, // 默认的圆角大小
+    size: SizeEnum.Default // 默认的头像大小
+  }, props);
+  
+  // 返回头像的DOM结构
   return (
     <div class={styles.avatar}
       style={{
-        "border-radius": `var( --border-radius-${merge.radius})`,
-        width: `var(--size-${merge.size})`,
-        height: `var(--size-${merge.size})`,
+        "border-radius": `var( --border-radius-${merge.radius})`, // 设置圆角大小
+        width: `var(--size-${merge.size})`, // 设置宽度
+        height: `var(--size-${merge.size})`, // 设置高度
       }}
     >
-      <img src={props.src} onError={(e) => e.currentTarget.src = 'https://avatars.githubusercontent.com/u/10290406?v=4'} />
+      <img src={props.src} onError={(e) => e.currentTarget.src = 'https://avatars.githubusercontent.com/u/10290406?v=4'} /> // 加载头像图片，错误时使用默认头像
     </div>
   )
-}
+};
