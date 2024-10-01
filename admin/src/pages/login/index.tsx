c78535e2956919f9816f2b01ed4eebbabcae5ce4
银永鑫 <1101278643@qq.com>
2024-10-02 00:59:59 +0800

    优化 404 页面设计并添加联系支持按钮
    在 404 页面中，移除了不必要的注释，增加了自定义的 404 错误图标，并调整了字体大小。同时，添加了一个联系支持的按钮，增加了用户在遇到问题时的互动性。此外，优化了页面布局和样式，提高了整体的用户体验。

--- admin/src/pages/login/index.tsx
+++ admin/src/pages/login/index.tsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 
 export const description =
-	"A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image.";
+	"一个带有两列的登录页面。第一列包含电子邮件和密码的登录表单。有一个忘记密码的链接和一个注册链接（如果您还没有账户）。第二列有一张封面图片。";
 
 export function Login() {
 	return (
@@ -13,14 +13,14 @@ export function Login() {
 			<div className="flex items-center justify-center py-12">
 				<div className="mx-auto grid w-[350px] gap-6">
 					<div className="grid gap-2 text-center">
-						<h1 className="text-3xl font-bold">Login</h1>
+						<h1 className="text-3xl font-bold">登录</h1>
 						<p className="text-balance text-muted-foreground">
-							Enter your email below to login to your account
+							请输入您的电子邮件以登录账户
 						</p>
 					</div>
 					<div className="grid gap-4">
 						<div className="grid gap-2">
-							<Label htmlFor="email">Email</Label>
+							<Label htmlFor="email">电子邮件</Label>
 							<Input
 								id="email"
 								type="email"
@@ -30,27 +30,27 @@ export function Login() {
 						</div>
 						<div className="grid gap-2">
 							<div className="flex items-center">
-								<Label htmlFor="password">Password</Label>
+								<Label htmlFor="password">密码</Label>
 								<Link
 									to="/forgot-password"
 									className="ml-auto inline-block text-sm underline"
 								>
-									Forgot your password?
+									忘记密码？
 								</Link>
 							</div>
 							<Input id="password" type="password" required />
 						</div>
 						<Button type="submit" className="w-full">
-							Login
+							登录
 						</Button>
 						<Button variant="outline" className="w-full">
-							Login with Google
+							使用谷歌登录
 						</Button>
 					</div>
 					<div className="mt-4 text-center text-sm">
-						Don&apos;t have an account?{" "}
+						还没有账户？{" "}
 						<Link to="#" className="underline">
-							Sign up
+							注册
 						</Link>
 					</div>
 				</div>
@@ -59,7 +59,7 @@ export function Login() {
 				<img
 					className="w-full object-cover "
 					src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
-					alt="Landscape photograph by Tobias Tullius"
+					alt="一张风景照片，由 Tobias Tullius 拍摄"
 				/>
 			</div>
 		</div>
