ca83000e7478beb9e4f19e59574a3be15177d0d0
银永鑫 <1101278643@qq.com>
2024-10-02 01:05:42 +0800

    重构axios配置，提高代码整洁度
    - 将axios配置和拦截器从App.tsx移动到main.tsx
    - 删除了App.tsx中的冗余代码
    - 优化了项目结构，提高了可读性和维护性

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,23 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import { client } from "./services";
-
-// configure internal service client
-client.setConfig({
-	// set default base url for requests
-	baseUrl: "http://localhost:3000",
-	// set default headers for requests
-	headers: {
-		Authorization: `Bearer ${localStorage.getItem("token")}`,
-	},
-});
-
-client.interceptors.response.use((response) => {
-	if (response.status === 200) {
-		console.log(`request to ${response.url} was successful`);
-	}
-	return response;
-});
 
 function App() {
 	return (
--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { client } from "./services";
 
-createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
-)
+// configure internal service client
+client.setConfig({
+	// set default base url for requests
+	baseUrl: "http://localhost:3000",
+	// set default headers for requests
+	headers: {
+		Authorization: `Bearer ${localStorage.getItem("token")}`,
+	},
+});
+
+client.interceptors.response.use((response) => {
+	if (response.status === 200) {
+		console.log(`request to ${response.url} was successful`);
+	}
+	return response;
+});
+
+createRoot(document.getElementById("root")!).render(
+	// <StrictMode>
+	<App />
+	// </StrictMode>,
+);
