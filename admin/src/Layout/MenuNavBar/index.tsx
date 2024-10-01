88c4f618a6595a85e0ebec0a960715663ad62026
银永鑫 <1101278643@qq.com>
2024-10-01 23:38:22 +0800

    更新MenuNavBar组件，替换默认Avatar为图片
    将MenuNavBar组件中的默认Avatar组件替换为一个带有图片的div。这涉及到：
    - 移除原有的Avatar组件及其子组件
    - 添加一个新的div，包含一个从Unsplash获取的图片
    - 为图片添加圆形边框和对象覆盖样式
    
    这个更改提升了用户界面的视觉效果，通过使用实际图片替代了之前的占位符Avatar组件。

--- admin/src/Layout/MenuNavBar/index.tsx
+++ admin/src/Layout/MenuNavBar/index.tsx
@@ -9,6 +9,7 @@ import UserInfo from "./UserInfo";
 import { cn } from "@/lib/utils";
 import styles from "./index.module.less";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 const MenuNavBar = () => {
 	const [menuTree, setMenuTree] =
@@ -44,10 +45,13 @@ const MenuNavBar = () => {
 		>
 			<Card className="flex justify-between flex-col gap-2 p-4 h-full">
 				<div>
-					<Avatar>
-						<AvatarImage src="https://github.com/shadcn.png" />
-						<AvatarFallback>CN</AvatarFallback>
-					</Avatar>
+					<div className="w-10 h-10">
+						<img
+							src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+							alt="Image"
+							className="rounded-md object-cover w-full h-full"
+						/>
+					</div>
 				</div>
 				<div className="flex flex-col h-full gap-2 overflow-y-auto scroll">
 					{menuMemo}
