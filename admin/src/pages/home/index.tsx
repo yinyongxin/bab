81fa1633500f1620c38c9729f15ef1e4305a518d
银永鑫 <1101278643@qq.com>
2024-10-02 21:54:56 +0800

    在首页添加样式测试内容
    在首页组件中添加了一系列新的样式测试，包括文本和背景颜色类。这将有助于确认样式类是否正确应用于元素。

--- admin/src/pages/home/index.tsx
+++ admin/src/pages/home/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { icons } from "lucide-react";
-import { Icon } from "@/components";
 
 const Home = () => {
 	const [count, setCount] = useState(0);
@@ -10,8 +9,16 @@ const Home = () => {
 	}, []);
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
+			<div className="bg-blue-500">
+				<div className="text-primary">text-primary</div>
+				<div className="text-primary-foreground">text-primary-foreground</div>
+				
+				<div className="bg-background">bg-background</div>
+				<div className="text-background">text-background</div>
+				<div className="bg-foreground">bg-foreground</div>
+				<div className="text-foreground">text-foreground</div>
+			</div>
 			<Button onClick={() => setCount(count + 1)}>
-				<Icon name="ascasc"></Icon>
 				<span className="text-2xl">{count}</span>
 			</Button>
 		</div>
