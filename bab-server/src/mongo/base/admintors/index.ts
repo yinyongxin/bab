9b05a047d4a5ca76be8f9dd101acffcec2186350
银永鑫 <1101278643@qq.com>
2024-10-05 00:59:00 +0800

    在 Admintors 文档中添加排序字段
    在 Admintors Mongoose 模型中添加了 sort 字段，用于菜单排序。该字段有一个默认值 0，并在 Swagger 文档中进行了相应的描述。这一变更旨在改善后台管理员界面中菜单项的排序逻辑。

--- bab-server/src/mongo/base/admintors/index.ts
+++ bab-server/src/mongo/base/admintors/index.ts
@@ -81,6 +81,16 @@ export class Admintors extends BaseDocument {
   @ApiProperty({ required: false, description: '邮箱', example: '' })
   email: string;
 
+  /**
+   * 排序
+   */
+  @Prop({
+    default: 0,
+  })
+  // swagger
+  @ApiProperty({ required: true, description: '用作菜单排序', example: 0 })
+  sort: number;
+
   @Prop({
     default: AdmintorStatusEnum.Open,
     required: true,
