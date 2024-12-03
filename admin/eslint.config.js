f61942e36c1e3d1082eab25d9e50d6b225cf1b91
银永鑫 <1101278643@qq.com>
2024-12-03 20:27:20 +0800

    build(admin): 调整 ESLint 配置并优化数据表格组件
    - 更新 ESLint 配置文件，增加对特定目录的忽略
    - 在 ESLint 规则中添加新的警告和禁用规则
    - 在数据表格组件中添加换行符，提高代码可读性

--- admin/eslint.config.js
+++ admin/eslint.config.js
@@ -1,28 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+	{ ignores: ["dist", "src/components/ui", ".vite"] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		files: ["**/*.{ts,tsx}"],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			"react-hooks": reactHooks,
+			"react-refresh": reactRefresh,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"react-refresh/only-export-components": [
+				"warn",
+				{ allowConstantExport: true },
+			],
+			"react-hooks/exhaustive-deps": "off",
+			// 注意：您必须禁用基本规则，因为它可能会报告不正确的错误
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+		},
+	}
+);
--- admin/src/components/data-table.tsx
+++ admin/src/components/data-table.tsx
@@ -1,3 +1,4 @@
+
 import {
 	ColumnDef,
 	flexRender,
