822ec0f104fffdc173eac89f33674bae5e794758
银永鑫 <1101278643@qq.com>
2024-12-03 23:03:18 +0800

    chore: 删除 new-tab 目录
    删除了 new-tab 目录下的所有文件和子目录，包括：
    - .gitignore
    - .hintrc
    - .vscode/settings.json
    - README.md
    - app.config.ts
    - package.json
    - public/favicon.ico
    - src 目录及其内容
    - tsconfig.json
    - vitest.config.ts

--- new-tab/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-declare namespace GlobalNS {
-	type ColorLevelType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-}
--- new-tab/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const toggleTheme = () => {
-	const theme = document.body.getAttribute("theme-mode");
-	if (theme === "dark") {
-		document.body.setAttribute("theme-mode", "light");
-	} else {
-		document.body.setAttribute("theme-mode", "dark");
-	}
-};
