fb4d185fe1c577c158c5990a1c4965b55b20f698
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-04-01 21:53:08 +0800

    update

--- admin/src/utils/index.ts
+++ admin/src/utils/index.ts
@@ -1,5 +1,6 @@
 import appConfig from '@/configs/app.config';
 
 export const getFilePath = (path: string) => {
+  if (path.startsWith('http')) return path;
   return `${appConfig.fileBaseUrl}${path}`;
 };
