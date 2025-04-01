7dabeb5cf6e7efa7cedf4c304cb6a00b0e56e3ec
yinyongxin <1101278643@qq.com>
2025-04-01 03:19:10 +0000

    update

--- admin/openapi-ts.config.ts
+++ admin/openapi-ts.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-  input:
-    'http://localhost:3000/api-json',
+  input: 'http://localhost:3000/api-json',
   output: {
     format: 'prettier',
     lint: 'eslint',
