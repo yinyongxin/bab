c5c401edd845f284059cbc2b671c5f421703009b
yinyongxin <1101278643@qq.com>
2025-04-01 10:33:04 +0000

    update

--- admin/src/store/hook.ts
+++ admin/src/store/hook.ts
@@ -1,8 +1,8 @@
-import { useDispatch, useSelector } from 'react-redux'
-import type { TypedUseSelectorHook } from 'react-redux'
-import { ThunkDispatch, AnyAction } from '@reduxjs/toolkit'
-import type { RootState } from './rootReducer'
+import { useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import { ThunkDispatch, Action } from '@reduxjs/toolkit';
+import type { RootState } from './rootReducer';
 
-export type AppThunkDispatch = ThunkDispatch<RootState, any, AnyAction>
-export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export type AppThunkDispatch = ThunkDispatch<RootState, any, Action>;
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
