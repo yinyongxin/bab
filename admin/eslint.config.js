3fbb804b9063d70da13c914230c9b9f3cef12263
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-04-05 10:01:07 +0800

    build(admin): 优化 ESLint 配置和脚本
    - 更新 ESLint 配置，增加对 Markdown 文件和 node_modules 目录的忽略
    - 修改 package.json 中的 ESLint 脚本，分离缓存和修复功能
    - 移除 ESLint 规则中的 no-console 警告

--- admin/eslint.config.js
+++ admin/eslint.config.js
@@ -3,7 +3,13 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   ...mantine,
-  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
+  {
+    ignores: [
+      '**/*.{mjs,cjs,js,d.ts,d.mts,md}',
+      './.storybook/main.ts',
+      './node_modules/**',
+    ],
+  },
   {
     files: ['**/*.story.tsx'],
     rules: { 'no-console': 'off' },
