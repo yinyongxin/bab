6030156ae7d907044e8916a1fabae16f65f00097
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-04-10 00:38:08 +0800

    style(admin): 优化加载屏幕和导航栏样式
    - 修改加载屏幕背景颜色为 mantine 主题颜色
    - 移除加载器颜色属性，使用默认蓝色
    - 调整导航栏链接组悬停和激活状态的样式
    - 优化代码格式和变量使用

--- admin/src/components/LoadingScreen/LoadingScreen.tsx
+++ admin/src/components/LoadingScreen/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import classes from './LoadingScreen.module.css';
 export default function LoadingScreen() {
   return (
     <div className={classes.loadingScreenBackground}>
-      <Loader style={{ alignSelf: 'center' }} color="blue" type="bars" />
+      <Loader style={{ alignSelf: 'center' }} type="bars" />
     </div>
   );
 }
