38c92ffdfd3e8a23c2f7f334d3b0112f17039707
yinyongxin <1101278643@qq.com>
2025-04-10 03:36:06 +0000

    update

--- admin/src/components/FontIcons.tsx
+++ admin/src/components/FontIcons.tsx
@@ -1,10 +1,17 @@
 type FontIconsProps = {
   style?: React.CSSProperties;
   name?: string;
+  size?: string | number;
+  color?: string;
 };
 const FontIcons = (props: FontIconsProps) => {
-  const { style, name } = props;
-  return <i style={style} className={`ti ti-${name}`} />;
+  const { style, name, size, color } = props;
+  return (
+    <i
+      style={{ fontSize: size, color: color, ...style }}
+      className={`ti ti-${name}`}
+    />
+  );
 };
 
 export default FontIcons;
--- admin/src/layout/HeaderArea/MegaMenu/MegaMenu.tsx
+++ admin/src/layout/HeaderArea/MegaMenu/MegaMenu.tsx
@@ -22,179 +22,136 @@ import {
   SimpleGrid,
   Text,
   ThemeIcon,
+  Title,
   UnstyledButton,
   useMantineTheme,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from './MegaMenu.module.css';
 import { useAppSelector } from '@/store';
-
-const mockdata = [
-  {
-    icon: IconCode,
-    title: 'Open source',
-    description: 'This Pokémon’s cry is very loud and distracting',
-  },
-  {
-    icon: IconCoin,
-    title: 'Free for everyone',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-  },
-  {
-    icon: IconBook,
-    title: 'Documentation',
-    description: 'Yanma is capable of seeing 360 degrees without',
-  },
-  {
-    icon: IconFingerprint,
-    title: 'Security',
-    description: 'The shell’s rounded shape and the grooves on its.',
-  },
-  {
-    icon: IconChartPie3,
-    title: 'Analytics',
-    description: 'This Pokémon uses its flying ability to quickly chase',
-  },
-  {
-    icon: IconNotification,
-    title: 'Notifications',
-    description: 'Combusken battles with the intensely hot flames it spews',
-  },
-];
+import { Link, useLocation } from 'react-router-dom';
+import { TreeMenuDataDto } from '@/client';
+import FontIcons from '@/components/FontIcons';
+import path from 'path';
+import { useState, useEffect } from 'react';
 
 export default function MegaMenu() {
   const { navigationTree } = useAppSelector((state) => state.auth.menus);
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
-    useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
   const theme = useMantineTheme();
+  const [activeSubLink, setActiveSubLink] = useState('');
+  const [activeMainLink, setActiveMainLink] = useState('');
+  const location = useLocation();
+  useEffect(() => {
+    const currentPath = location.pathname.split('/');
+    const currentMainLink = currentPath[1];
+    const currentSubLink = currentPath[2];
+    setActiveMainLink(currentMainLink);
+    setActiveSubLink(currentSubLink);
+  }, [location.pathname]);
 
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon size={22} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
+  const getLinks = (navigation: TreeMenuDataDto) => {
+    const { children } = navigation;
+    return children.map((item) => {
+      const active = activeSubLink === item.path;
+      console.log('activeSubLink', activeSubLink);
+      console.log(' item.path', item.path);
+      console.log('active', active);
+      return (
+        <Link
+          to={`/${navigation.path}/${item.path}`}
+          className={classes.subLink}
+          key={item.name}
+          data-active={active || undefined}
+        >
+          <Group wrap="nowrap" align="flex-start">
+            <ThemeIcon size={36} variant="default" radius="md">
+              <FontIcons
+                size={22}
+                color={theme.colors.gray[5]}
+                name={item.icon}
+              />
+            </ThemeIcon>
+            <div>
+              <Title order={6} lineClamp={1}>
+                {item.name}
+              </Title>
+              <Text size="xs" c="dimmed" lineClamp={1}>
+                {item.description}
+              </Text>
+            </div>
+          </Group>
+        </Link>
+      );
+    });
+  };
 
-  const getMainLinks = () => {};
+  const mainLinks = navigationTree.map((navigation) => {
+    const { children } = navigation;
+    const active = activeMainLink === navigation.path;
+    const hasChildren = children.length > 0;
+    if (hasChildren) {
+      return (
+        <HoverCard
+          width={600}
+          position="bottom"
+          radius="md"
+          shadow="md"
+          withinPortal
+        >
+          <HoverCard.Target>
+            <Link to={navigation.path || '/'} className={classes.link}>
+              <Group
+                align="center"
+                gap="xs"
+                c={active ? 'var(--mantine-primary-color-5)' : undefined}
+              >
+                <FontIcons name={navigation.icon} size={18}></FontIcons>
+                <Title order={5} mr={5}>
+                  {navigation.name}
+                </Title>
+                <IconChevronDown size={16} />
+              </Group>
+            </Link>
+          </HoverCard.Target>
+
+          <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
+            <Group justify="space-between">
+              <Title order={5}>{navigation.name}</Title>
+              <Anchor href="#" fz="xs">
+                {/* View all */}
+              </Anchor>
+            </Group>
+
+            <Divider my="sm" />
+
+            <SimpleGrid cols={2} spacing="md">
+              {getLinks(navigation)}
+            </SimpleGrid>
+
+            <div className={classes.dropdownFooter}>
+              <Text size="xs" c="dimmed">
+                {navigation.description}
+              </Text>
+            </div>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      );
+    }
+
+    return (
+      <Link to={navigation.path || '/'} className={classes.link}>
+        {navigation.name}
+      </Link>
+    );
+  });
 
   return (
     <>
       <div className={classes.header}>
-        <Group justify="space-between" h="100%">
-          <Group h="100%" gap={0} visibleFrom="sm">
-            <a href="#" className={classes.link}>
-              Home
-            </a>
-            <HoverCard
-              width={600}
-              position="bottom"
-              radius="md"
-              shadow="md"
-              withinPortal
-            >
-              <HoverCard.Target>
-                <a href="#" className={classes.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      Features
-                    </Box>
-                    <IconChevronDown size={16} color={theme.colors.blue[6]} />
-                  </Center>
-                </a>
-              </HoverCard.Target>
-
-              <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
-                <Group justify="space-between" px="md">
-                  <Text fw={500}>Features</Text>
-                  <Anchor href="#" fz="xs">
-                    View all
-                  </Anchor>
-                </Group>
-
-                <Divider my="sm" />
-
-                <SimpleGrid cols={2} spacing={0}>
-                  {links}
-                </SimpleGrid>
-
-                <div className={classes.dropdownFooter}>
-                  <Group justify="space-between">
-                    <div>
-                      <Text fw={500} fz="sm">
-                        Get started
-                      </Text>
-                      <Text size="xs" c="dimmed">
-                        Their food sources have decreased, and their numbers
-                      </Text>
-                    </div>
-                    <Button variant="default">Get started</Button>
-                  </Group>
-                </div>
-              </HoverCard.Dropdown>
-            </HoverCard>
-            <a href="#" className={classes.link}>
-              Learn
-            </a>
-            <a href="#" className={classes.link}>
-              Academy
-            </a>
-          </Group>
-          <Burger
-            opened={drawerOpened}
-            onClick={toggleDrawer}
-            hiddenFrom="sm"
-          />
+        <Group h="100%" gap={0}>
+          {mainLinks}
         </Group>
       </div>
-
-      <Drawer
-        opened={drawerOpened}
-        onClose={closeDrawer}
-        padding="md"
-        title="Navigation"
-        hiddenFrom="sm"
-      >
-        <ScrollArea h="100%">
-          <Divider my="sm" />
-          <a href="#" className={classes.link}>
-            Home
-          </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
-            <Center inline>
-              <Box component="span" mr={5}>
-                Features
-              </Box>
-              <IconChevronDown size={16} color={theme.colors.blue[6]} />
-            </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
-          <a href="#" className={classes.link}>
-            Learn
-          </a>
-          <a href="#" className={classes.link}>
-            Academy
-          </a>
-
-          <Divider my="sm" />
-
-          <Group justify="center" grow pb="xl" px="md">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
-          </Group>
-        </ScrollArea>
-      </Drawer>
     </>
   );
 }
